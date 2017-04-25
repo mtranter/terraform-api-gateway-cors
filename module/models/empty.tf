722251805b49b959871b593634c4f262f5acc367
Mark Tranter <mark.a.tranter@gmail.com>
2017-04-25 21:04:00 +1000

    Change model name generation

--- module/models/empty.tf
+++ module/models/empty.tf
@@ -1,6 +1,6 @@
 resource "aws_api_gateway_model" "empty_model" {
   rest_api_id  = "${var.rest_api_id}"
-  name         = "Empty_${replace(replace(var.content_type,"/","_"),"+","_")}"
+  name         = "Empty_${replace(replace(var.content_type,"/",""),"+","")}"
   description  = "Empty Schema"
   content_type = "${var.content_type}"
 
